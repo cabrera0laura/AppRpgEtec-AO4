e83d0ba7e5c13a294605efe22bd569aff8df8284
cabrera0laura <lucas56hoseok@gmail.com>
2025-09-23 21:10:30 -0300

    realização da atividade avaliativa do Professor Luis/cadastro de Arma

---
+++ ViewModels/Armas/ListagemArmaViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Armas;
+
+namespace AppRpgEtec.ViewModels.Armas
+{
+    internal class ListagemArmaViewModel : BaseViewModel
+    {
+       
+         private ArmaService aService;
+         public ObservableCollection<Arma> Armas { get; set; }
+         public ListagemArmaViewModel()
+         {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            aService = new ArmaService(token);
+            Armas = new ObservableCollection<Arma>();
+         }
+        // Proximos elementos da classe aqui.  PDF 5 - CONTINUAR .....
+
+       /* public async TasK ObterPersonagem()
+        {
+            try
+            {
+                Armas = await aService.GetArmasAsync();
+                //.........
+            }
+            catch(Exception)
+            {
+
+            }
+        }*/
+
+       
+
+    } // Fim da classe
+}
